2b0fe3d967628603d91bf8e15859498e5e344d47
vickysomtee <vickysomtee@gmail.com>
2022-03-04 23:22:26 +0100

    added comments

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const nodeCron = require("node-cron");
 //mongoose  schema
 const Photo = require("./photo.model");
 
+
 const PORT = process.env.PORT
 const dbConnect = process.env.MONGO_URI
 
+// node server setup
 const server = http.createServer(async (req, res) => {
   if ((req.url === "/api") & (req.method === "GET")) {
     res.writeHead(200, { "Content-Type": "application/json" });
@@ -20,7 +22,10 @@ const server = http.createServer(async (req, res) => {
     res.end();
   } else if ((req.url === "/api/photos") & (req.method === "GET")) {
     try {
+      //crone job for fetching and storing data every 1 minute
       nodeCron.schedule("*/1 * * * *", async () => {
+        
+        //fetch request from api
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/photos",
           {
@@ -31,7 +36,8 @@ const server = http.createServer(async (req, res) => {
         const data = await response.json();
         const photos = JSON.stringify( data );
         for (const photo of data) {
-  
+          
+          //store fetched data in mongo atlas
           await Photo.insertMany([{
               albumId: photo.albumId,
               id: photo.id,
@@ -51,6 +57,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
+//mongo connection
 mongoose
   .connect(dbConnect, {
     useUnifiedTopology: true,
@@ -61,6 +68,7 @@ mongoose
   })
   .catch((error) => console.log(error));
 
+  //server setup
 server.listen(PORT, () => {
   console.log("Server is running");
 });
